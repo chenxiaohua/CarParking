d0fe51d7482f29e3a047cdd256faba59f9fec7cd
chenxiaohua <lynnmill@hotmail.com>
2015-03-19 16:39:42 +0800

    add viewpage

--- src/com/car/carparking/view/Login.java
+++ src/com/car/carparking/view/Login.java
@@ -1,12 +1,25 @@
 package com.car.carparking.view;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Parcelable;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v4.view.ViewPager.OnPageChangeListener;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.Toast;
+import android.widget.ImageView.ScaleType;
 import android.content.Intent;
 
 import com.car.carparking.R;
@@ -24,10 +37,49 @@ public class Login extends Activity implements View.OnClickListener{
 	static private int INVALID_USER =2;
 	private TitleView mTitleView;
 	
+	private ViewPager mViewPager; 
+	private List<ImageView> mImageViews; 
+
+	private int[] mImageResId;
+	private List<View> mDots; 
+
+	private int mCurrentItem = 0;
+	
+	private ScheduledExecutorService mScheduledExecutorService;
+
+	private Handler mHandler = new Handler() {
+		public void handleMessage(android.os.Message msg) {
+			mViewPager.setCurrentItem(mCurrentItem);
+		};
+	};
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+        mImageResId = new int[] { R.drawable.a, R.drawable.a,R.drawable.a, R.drawable.a,R.drawable.a};
+
+		mImageViews = new ArrayList<ImageView>();
+
+		for (int i = 0; i < mImageResId.length; i++) {
+			ImageView imageView = new ImageView(this);
+			imageView.setImageResource(mImageResId[i]);
+			//imageView.setScaleType(ScaleType.CENTER_CROP);
+			imageView.setScaleType(ScaleType.FIT_XY);
+			mImageViews.add(imageView);
+		}
+
+		mDots = new ArrayList<View>();
+		mDots.add(findViewById(R.id.v_dot0));
+		mDots.add(findViewById(R.id.v_dot1));
+		mDots.add(findViewById(R.id.v_dot2));
+		mDots.add(findViewById(R.id.v_dot3));
+		mDots.add(findViewById(R.id.v_dot4));
+
+		mViewPager = (ViewPager) findViewById(R.id.vp);
+		mViewPager.setAdapter(new MyAdapter());
+		mViewPager.setOnPageChangeListener(new MyPageChangeListener());
+		
         mLoginButton = (ImageButton)findViewById(R.id.logimgButton);
         mLoginButton.setOnClickListener( this);
         mUserText = (EditText)findViewById(R.id.username);
@@ -36,6 +88,105 @@ public class Login extends Activity implements View.OnClickListener{
         mTitleView.setTitle(R.string.app_name); 
         mTitleView.setMenuVisible(View.GONE);
     }
+    private class ScrollTask implements Runnable {
+
+		public void run() {
+			synchronized (mViewPager) {
+				//mCurrentItem = (mCurrentItem + 1) % mImageViews.size();
+				mCurrentItem++;
+				mHandler.obtainMessage().sendToTarget(); 
+			}
+		}
+
+	}
+
+	private class MyPageChangeListener implements OnPageChangeListener {
+		private int oldPosition = 0;
+
+		/**
+		 * This method will be invoked when a new page becomes selected.
+		 * position: Position index of the new selected page.
+		 */
+		public void onPageSelected(int position) {
+			mCurrentItem = position;
+			position = position % mImageViews.size();
+			//mCurrentItem = position;
+			mDots.get(oldPosition).setBackgroundResource(R.drawable.dot_normal);
+			mDots.get(position).setBackgroundResource(R.drawable.dot_focused);
+			oldPosition = position;
+		}
+
+		public void onPageScrollStateChanged(int arg0) {
+
+		}
+
+		public void onPageScrolled(int arg0, float arg1, int arg2) {
+			
+		}
+	}
+
+	private class MyAdapter extends PagerAdapter {
+		@Override
+		public int getCount() {
+			//return mImageResId.length;
+			return Integer.MAX_VALUE;
+		}
+
+		@Override
+		public Object instantiateItem(View arg0, int arg1) {
+            int index = arg1 % mImageViews.size();
+            ImageView v = mImageViews.get(index);
+            if ( index%2 == 0 ) {
+                v.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        //
+                    }
+                });
+            }else{
+                v.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        //
+                    }
+                });
+            }
+			((ViewPager) arg0).addView(v);
+            return v;
+			//return mImageViews.get(arg1% mImageViews.size());
+		}
+
+		@Override
+		public void destroyItem(View arg0, int arg1, Object arg2) {
+			((ViewPager) arg0).removeView(mImageViews.get(arg1%mImageViews.size()));
+		}
+
+		@Override
+		public boolean isViewFromObject(View arg0, Object arg1) {
+			return arg0 == arg1;
+		}
+
+		@Override
+		public void restoreState(Parcelable arg0, ClassLoader arg1) {
+
+		}
+
+		@Override
+		public Parcelable saveState() {
+			return null;
+		}
+
+		@Override
+		public void startUpdate(View arg0) {
+
+		}
+
+		@Override
+		public void finishUpdate(View arg0) {
+
+		}
+	}
+
     
     @Override
 	public void onClick(View view) {
@@ -72,6 +223,12 @@ public class Login extends Activity implements View.OnClickListener{
         }
     	return INVALID_NULL;
     }
+	@Override
+	protected void onStart() {
+		mScheduledExecutorService = Executors.newSingleThreadScheduledExecutor();
+		mScheduledExecutorService.scheduleAtFixedRate(new ScrollTask(), 1, 10, TimeUnit.SECONDS);
+		super.onStart();
+	}
     @Override
     protected void onResume() {
         super.onResume();  
@@ -84,6 +241,7 @@ public class Login extends Activity implements View.OnClickListener{
 
     @Override
     protected void onStop() {
+    	mScheduledExecutorService.shutdown();
         super.onStop();
     }
 
